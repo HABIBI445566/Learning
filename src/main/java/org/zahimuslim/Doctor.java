49fd8db40be3b7dbfdab7c6a302e4d73349d806f
habib <habib@zktecodev.com>
2024-09-26 19:03:19 +0530

    -> Removed all the code from xml Start of Annotations

--- src/main/java/org/zahimuslim/Doctor.java
+++ src/main/java/org/zahimuslim/Doctor.java
@@ -4,85 +4,19 @@ import org.springframework.stereotype.Component;
 
 import javax.print.Doc;
 
-// we are telling spring that this is a component and can be used to create a componenet out of
-// this will fail if we dont mention this in the spring.xml file.
-
 @Component
 public class Doctor implements Staff{
-
-    //over here we are injecting using constructor injection.
-    //         <constructor-arg value="MBBS"></constructor-arg>
-/*public Doctor(String qualification){
-    this.qualification = qualification;
-}*/
-    /*
-    We are going to get another property called qualification
-    now spring doesnt know about this property and we have getters and
-    setters for it, so its simply is going to put null for it.
-
-e values to it we can go it from the xml file itself
-
-     */
-
-
-    /*
-
-    This is for setter injection
-
-            <property name="qualification" value="MBBS"></property>
-
-     */
-
     private String qualification;
-
-
-
     public void assist()
     {
         System.out.println("Doctor is assisting");
     }
-
-
-    //I am commenting this out while trying to learn about annotination for simplifiction
-   /* public String getQualification() {
-        return qualification;
-    }
-
-    public void setQualification(String qualification) {
-        this.qualification = qualification;
-    }*/
-
-
-    /*
-    Okay for simplicity sake we are going to create a nurse object over here and get it injected
-     */
-
-    /*This was only for a demo if you want to see how we are injecting
-            using id's just look at the spring.xml file*/
-
-   /* private Nurse nurse;
-
-    public Nurse getNurse() {
-        return nurse;
-    }
-
-    public void setNurse(Nurse nurse) {
-        this.nurse = nurse;
-    }*/
-
-   // 26-09-24
-
-//   we are going to add a constructor to the doctor class and we shall inject it from spring.xml
-
     public Doctor(String qualification) {
         this.qualification = qualification;
     }
-
-
     public String getQualification() {
         return qualification;
     }
-
     public void setQualification(String qualification) {
         this.qualification = qualification;
     }
--- src/main/java/org/zahimuslim/Main.java
+++ src/main/java/org/zahimuslim/Main.java
@@ -5,40 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.print.Doc;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-
-        // I am making changes to the main projects to check if my push was succssfull or not
-
-
-
-
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        // there are many ways to get this context but we shall go with xml
-        //based configration
-
-        // everything we did till now we have used constructor and setter injection
-        // this wont work beacause we have not defined this in the xml
-       // Doctor doctor = context.getBean(Doctor.class);
-        // we can get this object by using the ID
-       // Nurse nurse = (Nurse) context.getBean("nurse");
-        //we have created an interface called Staff, which the doc and then nurse both implements
-        // now we can write thing this way.
-        Doctor staff = context.getBean(Doctor.class);
-        // this way we have decopled the entire application.
-        //staff.assist();
-        // nurse.assist();
-       /* Doctor doc = new Doctor();*/
-        //doctor.assist();
-        // Doctor docqal = context.getBean(Doctor.class);
-        System.out.println(staff.getQualification());
-        staff.assist();
-
-
-        //we shall move to annotions now, I will be commenting all the beans from the spring.xml file.
-        //everything till here was XML, the MBBS one was missed IG
-
     }
 }
