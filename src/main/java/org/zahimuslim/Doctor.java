cd9d4bae8101aefd6289d7112bd912f9baeb000a
habib <habib@zktecodev.com>
2024-09-26 19:10:21 +0530

    -> There are many annotations where we have to learn here they are moving to java config.

--- src/main/java/org/zahimuslim/Doctor.java
+++ src/main/java/org/zahimuslim/Doctor.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 
 import javax.print.Doc;
 
+
+//to use annotations we need to use @Component , whatever is marked with this we are telling beans that you can use it to make beans out of it.
 @Component
 public class Doctor implements Staff{
     private String qualification;
@@ -11,13 +13,5 @@ public class Doctor implements Staff{
     {
         System.out.println("Doctor is assisting");
     }
-    public Doctor(String qualification) {
-        this.qualification = qualification;
-    }
-    public String getQualification() {
-        return qualification;
-    }
-    public void setQualification(String qualification) {
-        this.qualification = qualification;
-    }
+
 }
--- src/main/java/org/zahimuslim/Main.java
+++ src/main/java/org/zahimuslim/Main.java
@@ -1,5 +1,6 @@
 package org.zahimuslim;
 
+import com.sun.source.doctree.DocTree;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,5 +10,7 @@ import javax.print.Doc;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Staff staff = context.getBean(Doctor.class);
+        staff.assist();
     }
 }
