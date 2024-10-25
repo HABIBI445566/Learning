9b3198f8bfd651a3a1414348fc020a75246efdf6
habib <habib@zktecodev.com>
2024-10-25 08:56:38 +0530

    -> start of Spring Data JPA

--- src/main/java/com/zahimuslim/Spring_boot_learning/SpringBootLearningApplication.java
+++ src/main/java/com/zahimuslim/Spring_boot_learning/SpringBootLearningApplication.java
@@ -8,7 +8,7 @@ public class SpringBootLearningApplication {
 
 	public static void main(String[] args) {
 
-		//start of Testing
+		//start of Spring Data JPA
 		SpringApplication.run(SpringBootLearningApplication.class, args);
 
 	}
