aa1bd6cf61b0d44254517e303dfc7d5849e5c5ae
habib <habib@zktecodev.com>
2024-11-05 19:59:40 +0530

    -> 6:37:45 5:43:15

--- src/test/java/com/zahimuslim/Spring_boot_learning/repository/CourseRepositoryTest.java
+++ src/test/java/com/zahimuslim/Spring_boot_learning/repository/CourseRepositoryTest.java
@@ -5,6 +5,9 @@ import com.zahimuslim.Spring_boot_learning.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -35,12 +38,74 @@ class CourseRepositoryTest {
                 .build();
 
         Course course = Course.builder()
-                        .title("Python")
+                        .title("NLP")
                                 .credit(8)
                                         .teacher(teacher)
                                                 .build();
 
         courseRepository.save(course);
+
+    }
+
+    //hch get comments on this and deep explanation
+    @Test
+    public void findAllPagination()
+    {
+
+        Pageable firstPageWithThreeRecords = PageRequest.of(0,3);
+        Pageable secondPageWithTwoRecords = PageRequest.of(1,2);
+
+       /* Long totalElements = courseRepository.findAll(firstPageWithThreeRecords).getTotalElements();
+        System.out.println("Total Elements -->"+ totalElements);
+
+        int totalPages = courseRepository.findAll(firstPageWithThreeRecords).getTotalPages();
+        System.out.println("Total Pages -->"+ totalPages);
+
+
+        List<Course> courses = courseRepository.findAll(firstPageWithThreeRecords).getContent();
+        System.out.println("courses -->"+ courses);
+
+Total Elements -->5
+Total Pages -->2
+courses -->[Course(courseId=1, title=Python, credit=8, courseMaterial=null, teacher=Teacher(teacherId=1, firstName=Vinay, lastName=B)), Course(courseId=2, title=DBA, credit=8, courseMaterial=null, teacher=Teacher(teacherId=2, firstName=Vinay, lastName=B)), Course(courseId=3, title=DSA, credit=8, courseMaterial=null, teacher=Teacher(teacherId=3, firstName=Vinay, lastName=B))]
+
+
+        */
+
+        Long totalElements = courseRepository.findAll(secondPageWithTwoRecords).getTotalElements();
+        System.out.println("Total Elements -->"+ totalElements);
+
+        int totalPages = courseRepository.findAll(secondPageWithTwoRecords).getTotalPages();
+        System.out.println("Total Pages -->"+ totalPages);
+
+
+        List<Course> courses = courseRepository.findAll(secondPageWithTwoRecords).getContent();
+        System.out.println("courses -->"+ courses);
+
+       /* Total Elements -->5
+        Total Pages -->3
+        courses -->[Course(courseId=3, title=DSA, credit=8, courseMaterial=null, teacher=Teacher(teacherId=3, firstName=Vinay, lastName=B)), Course(courseId=4, title=Java, credit=8, courseMaterial=null, teacher=Teacher(teacherId=4, firstName=Vinay, lastName=B))]
+
+
+*/
+
+
+    }
+    @Test
+    public void findAllWithSorting()
+    {
+        Pageable sortByTitle = PageRequest.of(0,6, Sort.by("title"));
+        Pageable sortByCreditDec = PageRequest.of(0,6, Sort.by("credit").descending());
+        Pageable sortByTitleAndCreditDesc = PageRequest.of(0,6, Sort.by("title").descending().and(Sort.by("credit")));
+
+        // i am not getting corse one for this
+        List<Course> listOfsortByTitle = courseRepository.findAll(sortByTitle).getContent();
+        System.out.println("listOfsortByTitle -->"+listOfsortByTitle);
+        List<Course> listOfsortByCreditDec = courseRepository.findAll(sortByCreditDec).getContent();
+        System.out.println("listOfsortByCreditDec -->"+listOfsortByCreditDec);
+        List<Course> listOfsortByTitleAndCreditDesc = courseRepository.findAll(sortByTitleAndCreditDesc).getContent();
+        System.out.println("listOfsortByTitleAndCreditDesc -->"+listOfsortByTitleAndCreditDesc);
+
 
     }
 
