763edb9a6592ed666531e1bd216d7177c4e8e262
habib <habib@zktecodev.com>
2024-11-06 09:28:22 +0530

    -> Committing deleted files

--- src/main/java/com/zahimuslim/Spring_boot_learning/repository/CourseMaterialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zahimuslim.Spring_boot_learning.repository;
-
-import com.zahimuslim.Spring_boot_learning.entity.CourseMaterial;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseMaterialRepository extends JpaRepository<CourseMaterial,Long> {
-}
