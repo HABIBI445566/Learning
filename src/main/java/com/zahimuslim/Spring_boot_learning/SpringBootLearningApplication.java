e86acba68518b4e4247a11cdd18687b9882c27b0
habib <habib@zktecodev.com>
2024-11-06 09:26:24 +0530

    -> DELETING EVERYTHING START OF SPRING SECURITY

--- src/main/java/com/zahimuslim/Spring_boot_learning/SpringBootLearningApplication.java
+++ src/main/java/com/zahimuslim/Spring_boot_learning/SpringBootLearningApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootLearningApplication {
 	public static void main(String[] args) {
-		//Will remove everything and beginning of the new project
+		//Will remove everything and beginning of the new project Done Deleting everything
 		SpringApplication.run(SpringBootLearningApplication.class, args);
 	}
 }
